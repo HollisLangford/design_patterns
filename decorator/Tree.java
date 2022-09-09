bf00830c3a349c11c70f6eef4018a6088268a2eb
hollis langford <hollislangford1@gmail.com>
2022-09-09 15:19:19 -0400

    decorator

---
+++ decorator/Tree.java
@@ -0,0 +1,16 @@
+package decorator;
+
+import java.util.ArrayList;
+
+public class Tree {
+    protected ArrayList<String> lines;
+
+    public Tree(ArrayList<String> lines){
+        this.lines = lines;
+    }
+
+    public String toString(ArrayList<String> lines){
+        return toString(lines);
+    }
+    
+}
